1120bd6ddf982bfd1e5bca8e0b767574cd458aaa
Terts Diepraam <terts.diepraam@gmail.com>
2018-11-29 19:18:28 +0100

    added padding for minutes and seconds

--- doomsday.js
+++ doomsday.js
@@ -7,6 +7,15 @@ window.onload = () => {
     setInterval(update_time, 1000);
 }
 
+function pad(text) {
+    text = text.toString()
+    if (text.length == 1) {
+        return "0" + text
+    } else {
+        return text
+    }
+}
+
 function update_time() {
     let current_time = new Date();
     let diff = target_date - current_time;
@@ -18,5 +27,6 @@ function update_time() {
         day_text = "days"
     }
     let diffdate = new Date(diff);
-    clock_text.innerHTML = days + " " + day_text + "<br>" + diffdate.getHours() + ":" + diffdate.getMinutes() + ":" + diffdate.getSeconds();
+
+    clock_text.innerHTML = days + " " + day_text + "<br>" + diffdate.getHours() + ":" + pad(diffdate.getMinutes()) + ":" + pad(diffdate.getSeconds());
 }
