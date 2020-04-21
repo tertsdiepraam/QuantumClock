404abdcd268b38e7c5e988d7a89fa1e56e287a16
Terts Diepraam <terts.diepraam@gmail.com>
2020-04-21 19:38:48 +0200

    Sandy & Floris, stop asking for features

--- doomsday.js
+++ doomsday.js
@@ -1,18 +1,23 @@
 var clock_text;
-var target_date = new Date("December 20, 2018 15:00:00");
+var name_text;
+
+var params = (new URL(document.location)).searchParams;
+var target_date = new Date(params.get('date'));
 
 window.onload = () => {
+    name_text = document.getElementById("name")
+    name_text.innerHTML = params.get("name")
     clock_text = document.getElementById("clock");
     update_time();
     setInterval(update_time, 1000);
 }
 
 function pad(text) {
-    text = text.toString()
+    text = text.toString();
     if (text.length == 1) {
-        return "0" + text
+        return "0" + text;
     } else {
-        return text
+        return text;
     }
 }
 
